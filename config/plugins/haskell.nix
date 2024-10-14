a8eda00ce97c20465a4be6a2505ffbb4a72ea9f5
cringulya <s0sis0n@yandex.ru>
2024-10-14 03:19:01 +0300

    config fix

--- config/plugins/haskell.nix
+++ config/plugins/haskell.nix
@@ -18,8 +18,6 @@
         vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
         -- Evaluate all code snippets
         vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
-
-        ht.repl.toggle(vim.api.nvim_buf_get_name(0)) end, opts)
       end
     '';
   }];
