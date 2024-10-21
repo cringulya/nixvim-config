3d70c22bb92408510e04b3bd53304718fa1794d1
cringulya <s0sis0n@yandex.ru>
2024-10-22 02:48:52 +0300

    updates

--- config/gui.nix
+++ config/gui.nix
@@ -5,7 +5,7 @@
 
         vim.g.neovide_refresh_rate = 60
         -- vim.g.neovide_fullscreen = 1
-        vim.opt.linespace = 12
+        -- vim.opt.linespace = 12
 
         -- Change scale
         vim.g.neovide_scale_factor = 1.5
