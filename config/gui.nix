8ac7ccbcd6c262c34f3d401d2f7e8305d1b57b78
cringulya <s0sis0n@yandex.ru>
2024-10-22 23:08:15 +0300

    updates

--- config/gui.nix
+++ config/gui.nix
@@ -13,21 +13,11 @@
           vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
         end
 
-        vim.o.clipboard = ""
-        vim.keymap.set("v", "<C-c>", '"+y') -- Copy
-        vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
-        vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
-        vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
-        vim.keymap.set("i", "<C-v>", "<c-r>+") -- Paste insert mode
-        -- Allow clipboard copy paste in neovim
-        vim.api.nvim_set_keymap("", "<C-v>", "+p<CR>", { noremap = true, silent
-        = true})
-        vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent =
-        true})
-        vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent =
-        true})
-        vim.api.nvim_set_keymap("v", "<C-v>", "<C-R>+", { noremap = true, silent =
-        true})
+        vim.keymap.set('v', '<c-c>', '"+y') -- Copy
+        vim.keymap.set('n', '<c-v>', '"+P') -- Paste normal mode
+        vim.keymap.set('v', '<c-v>', '"+P') -- Paste visual mode
+        vim.keymap.set('c', '<c-v>', '<C-R>+') -- Paste command mode
+        vim.keymap.set('i', '<c-v>', '<ESC>l"+Pli') -- Paste insert mode
 
         if vim.fn.has("macunix") then
           vim.g.neovide_input_macos_alt_is_meta = false
